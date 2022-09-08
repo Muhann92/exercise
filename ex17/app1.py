63b49e597a992f6450a98ac00eb52ba7871fdea4
Muhann92 <muhannadshikhani@gmail.com>
2022-09-08 17:52:41 +0200

    app1 final update

--- ex17/app1.py
+++ ex17/app1.py
@@ -1,27 +1,32 @@
 import sys
 import time
 
-print("\nName of Python script:",sys.argv[0])
-time.sleep(0.5)
+speed = float(0.5)
+time.sleep(speed)
+if "--fast"in sys.argv[1:]:
+    speed = 0
+    print("Fast mode enabled!")
+else:
+    print("Slow mode enabled")
 
+time.sleep(speed)
+print("\nName of Python script:",sys.argv[0])
+
+time.sleep(speed)
+print(f"\nEntered Arguments: {' - '.join(sys.argv[1:])}\n")
+
+time.sleep(speed)
 arg_len = len(sys.argv)
 print("Total arguments passed",arg_len)
-time.sleep(0.5)
 
-for i in range(1,arg_len):
-    if sys.argv[i] == "--help":
-        print("options:\
-             \n--help    shows Help message and will exit\
-             \n--fast    to activate Fast Mode\
-             \nslow mode is by default!")
-        break
-time.sleep(0.5)
+time.sleep(speed)
+if  "--help" in sys.argv[1:]:
+    print("\noptions:\n\n--help    shows Help message and will exit")
+    print("\n--fast    to activate Fast Mode")
+    print("\nslow mode is by default enabled!")
 
-for i in range(1,arg_len):
-    if sys.argv[i] == "--fast":
-        time.sleep(0)
-        print("\n(fast mode enabled)")
-    else:
-        print("\n(slow mode enabled)")
-        break
+
+    
+
+
     
